a486250ab232988303cb6273ba9632d423a8a2c1
Aditi Sahay <133731264+aditisahayad@users.noreply.github.com>
2026-08-23 20:42:29 +0530

    Time: 58 ms (54.71%) | Memory: 19.9 MB (28.24%) - LeetSync

---
+++ 2039-sum-game/sum-game.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        n = len(num)
+        half = n // 2
+
+        left_sum = 0
+        right_sum = 0
+        left_q = 0
+        right_q = 0
+
+        for i in range(half):
+            if num[i] == '?':
+                left_q += 1
+            else:
+                left_sum += int(num[i])
+
+        for i in range(half, n):
+            if num[i] == '?':
+                right_q += 1
+            else:
+                right_sum += int(num[i])
+
+        # Odd number of '?' means Alice can force a win
+        if (left_q + right_q) % 2 == 1:
+            return True
+
+        # Difference between fixed digits
+        diff = left_sum - right_sum
+
+        # Difference that '?' can compensate
+        required = 9 * (right_q - left_q) // 2
+
+        # Bob wins only if exact equality is possible
+        return diff != required
