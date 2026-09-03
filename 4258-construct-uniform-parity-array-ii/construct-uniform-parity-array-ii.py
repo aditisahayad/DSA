ef4c0d9308578d030de0655181ed84109211c19f
Aditi Sahay <133731264+aditisahayad@users.noreply.github.com>
2026-09-03 20:47:04 +0530

    Time: 20 ms (76.83%) | Memory: 35.3 MB (73.17%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+
+        mn = min(nums1)
+
+        # If minimum is odd, we can make every element odd
+        if mn % 2 == 1:
+            return True
+
+        # Minimum is even, so every element must already be even
+        for x in nums1:
+            if x % 2 == 1:
+                return False
+
+        return True
+            
